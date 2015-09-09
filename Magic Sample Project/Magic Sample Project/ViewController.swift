08a98ac39c2a32b11dde672c8b15a5f36c0a4617
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-09-09 03:05:21 -0400

    Renamed to Magic. Updated to Swift 2.0

---
+++ Magic Sample Project/Magic Sample Project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Magic Sample Project
+//
+//  Created by Arthur Sabintsev on 9/9/15.
+//  Copyright © 2015 Arthur Sabintsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        magic("This is a test string.")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Magic.swift
@@ -0,0 +1,29 @@
+//
+//  Magic.swift
+//
+//  Created by Arthur Sabintsev on 1/28/15.
+//  Copyright (c) 2015 Arthur Ariel Sabintsev. All rights reserved.
+//
+
+import Foundation
+
+/**
+    Writes the textual representation of `object` and a newline character into the standard output.
+    The textual representation is obtained from the `object` using its protocol conformances,
+    in the following order of preference: `Streamable`, `Printable`, `DebugPrintable`.
+
+    This function also augments the original function with the filename, function name, and line number of the object that is being logged.
+
+    - parameter object:   A textual representation of the object.
+    - parameter file:     Defaults to the name of the file that called magic(). Do not override this default.
+    - parameter function: Defaults to the name of the function within the file in which magic() was called. Do not override this default.
+    - parameter line:     Defaults to the line number within the file in which magic() was called. Do not override this default.
+
+*/
+public func magic<T>(object: T, _ file: String = __FILE__, _ function: String = __FUNCTION__, _ line: Int = __LINE__)
+{
+    let fileString = file as NSString
+    let fileLastPathComponent = fileString.lastPathComponent as NSString
+    let filename = fileLastPathComponent.stringByDeletingPathExtension
+    print("\(filename).\(function)[\(line)]: \(object)\n", terminator: "")
+}
